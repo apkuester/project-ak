f62c54383bcb7c6022d1f9e328eb7e3daed21e11
Adam Kuester <adam.p.kuester@gmail.com>
2014-07-20 09:59:10 -0400

    commented the script and subtracted 1 from n in for loop

--- mean.py
+++ mean.py
@@ -5,6 +5,6 @@ n = 0
 #Sum import values
 for num in sys.stdin: #each line is going into a num
 	sum += float(num) #convert number to float
-	n += 1 #add a number to each number
+	n -= 1 #subtract a number from each number
 	
-print sum/n #print the end sum divided by the number of data
+print sum/n #print the end sum divided by the n
