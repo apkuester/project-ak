3412ea8722c68700d1c0716d9b02d3a0371d9ee0
Anuli Anyanwu <canuli82@gmail.com>
2014-07-20 11:35:47 -0400

    adding code

--- mean.py
+++ mean.py
@@ -6,6 +6,6 @@ n = 0
 #Sum import values
 for num in open('data.txt'): #open each file and look at each line in the file
 	sum += float(num) #convert number to float
-	n += 1 #add a number to each number
+	n += 2  #add a  number 2 to each number
 	
 print sum/n #print the end sum divided by the n
