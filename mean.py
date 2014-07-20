16aafaf5df8de1f31881eb89eb2fe70f1ab0189a
Adam Kuester <adam.p.kuester@gmail.com>
2014-07-20 09:37:49 -0400

    add initial file to project

---
+++ mean.py
@@ -0,0 +1,10 @@
+
+import sys #library sys
+sum = 0
+n = 0
+
+for num in sys.stdin: #each line is going into a num
+	sum += float(num) #convert number to float
+	n += 1 #add a number to each number
+	
+print sum/n #print the end sum divided by the number of data
